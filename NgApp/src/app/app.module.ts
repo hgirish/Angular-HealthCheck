1582900afd206999f968c5c112fbc3d3c5ce5d70
Girish <girish@post.com>
2021-03-09 15:10:54 -0800

    health-check.component

--- NgApp/src/app/app.module.ts
+++ NgApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    HealthCheckComponent
   ],
   imports: [
     BrowserModule,
